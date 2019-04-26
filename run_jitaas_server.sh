a80efb4d3833f8902d91f25fd6f0e1243de29f9c
Harry Yu <harryyu1994@gmail.com>
2019-04-26 09:09:16 -0700

    modified docker-compose with mem_limit, cpu_quota

--- run_jitaas_server.sh
+++ run_jitaas_server.sh
@@ -1,1 +1,1 @@
-docker run --net=microservice --env TR_PrintJITaaSConnStats=1 --cpuset-cpus="0,1" --privileged -p 38400:38400 -it --rm -v /opt/ibm/java:/opt/ibm/java --name jitaas_server jitaas_server
+docker run --net=microservice --env TR_PrintJITaaSConnStats=1 --cpuset-cpus="7" --cpus=1 --privileged -p 38400:38400 -it --rm -v /opt/ibm/java:/opt/ibm/java --name jitaas_server jitaas_server
