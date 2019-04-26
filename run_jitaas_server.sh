7da7a6a23660a9d4846a693e202090d840408076
Harry Yu <harryyu1994@gmail.com>
2019-04-26 11:57:45 -0700

    Fix things

--- run_jitaas_server.sh
+++ run_jitaas_server.sh
@@ -1,1 +1,1 @@
-docker run --net=microservice --env TR_PrintJITaaSConnStats=1 --cpuset-cpus="7" --cpus=1 --privileged -p 38400:38400 -it --rm -v /opt/ibm/java:/opt/ibm/java --name jitaas_server jitaas_server
+docker run --net=microservice --env TR_PrintJITaaSConnStats=1 --cpuset-cpus="4,5,6,7" --cpus=4 --privileged -p 38400:38400 -it --rm -v /opt/ibm/java:/opt/ibm/java --name jitaas_server_microservice jitaas_server_microservice
